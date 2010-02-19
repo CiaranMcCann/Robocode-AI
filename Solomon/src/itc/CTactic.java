ae80ecca7bd73b73ab0b6de4bf9ac3ff8a6c08a9
CiaranMcCann <ciaran@flax.ie>
2010-02-19 22:13:39 +0000

    Added AI class file and now working on isGoodTactic() in CTactic class.

---
+++ Solomon/src/itc/CTactic.java
@@ -0,0 +1,22 @@
+package itc;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CTactic {
+
+	
+	protected List gaugingList = new ArrayList();
+	
+	/**
+	 * Temp method, more to add only for dev perpoese
+	 * @return
+	 */
+	public boolean isGoodTactic()
+	{
+		
+		this.gaugingList.add(1);
+		
+		return true;
+	}
+}
