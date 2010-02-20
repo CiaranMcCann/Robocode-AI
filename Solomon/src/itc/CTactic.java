440ee4762f3a56e057f87cc96aafe3b52639288f
CiaranMcCann <ciaran@flax.ie>
2010-02-20 17:01:41 +0000

    7 hours work on firday

--- Solomon/src/itc/CTactic.java
+++ Solomon/src/itc/CTactic.java
@@ -1,22 +1,59 @@
 package itc;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import robocode.HitByBulletEvent;
+import robocode.ScannedRobotEvent;
 
 
 public class CTactic {
 
 	
-	protected List gaugingList = new ArrayList();
+	//This is the sucess thershold that a tactic most to be labelled as efftive 
+	protected final byte GAUGING_THERSHOLD = 60; 
+	//protected List gaugingList = new ArrayList();
+	protected List<Byte> gaugingList  = new ArrayList<Byte>();
+
+	
+	public void run(solomon s)
+	{
+		
+	}
+	
+
+	public void onScannedRobot_(solomon s, ScannedRobotEvent e)
+	{
+		
+	}
+	
+	
+	public void onHitByBullet_(solomon s, HitByBulletEvent e)
+	{
+		
+	}
 	
 	/**
-	 * Temp method, more to add only for dev perpoese
+	 * Calcuates the effecncey of the tactic using the gaugingList
+	 * and returns true and false
 	 * @return
 	 */
 	public boolean isGoodTactic()
 	{
+		boolean result = false;
+		int sumOfGauging = 0;		
+		int sumOfElements = gaugingList.size();
 		
-		this.gaugingList.add(1);
+		for(int i = 0; i < sumOfElements; i++)
+		{
+		  sumOfGauging += gaugingList.get(i);
+		}
 		
-		return true;
+		sumOfGauging = (sumOfGauging/sumOfElements)*100; 
+		
+		if(sumOfGauging < GAUGING_THERSHOLD)
+		{
+			result = true;
+		}
+		
+		return result;
 	}
 }
--- Solomon/src/itc/solomon.java
+++ Solomon/src/itc/solomon.java
@@ -1,41 +1,123 @@
 package itc;
+import itc.CTactic;
+import itc.AI;
+import itc.tactic.CTactic_ea0;
 import robocode.*;
-//import java.awt.Color;
+import java.sql.Time;
+import java.sql.Timestamp;
 
 /**
  * Solomon - a robot by (your name here)
  */
 public class solomon extends Robot
 {
+	private byte status;
+	public byte getStatus() {
+		return status;
+	}
+	public void setStatus(byte status) {
+		this.status = status;
+	}
+
+	private int currentTacticIndex;
+	private CTactic tacticLibrary[][];
+	
+	private int healthBeforeTactic;
+	
+	private final int E_AGGRESSIVE = 85;
+	private final int AGGRESSIVE= 65;
+	private final int DEFENSIVE = 45;
+	//private final int E_DEFENSIVE = 25;
+	
+	public Time gameTimer = new Time (System.currentTimeMillis()); 
+	public Time gameTimer2 = new Time (System.currentTimeMillis()); 
+
+	
+	public solomon()
+	{
+		status = 0;
+		currentTacticIndex = 0;
+		tacticLibrary = new CTactic[3][3];
+		
+		tacticLibrary[0][0] = new CTactic_ea0();
+		
+		healthBeforeTactic = 0;
+		
+		
+	}
+		
 	/**
 	 * run: Solomon's default behavior
 	 */
+
 	public void run() {
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-		//setColors(Color.red,Color.blue,Color.green);
+						
 		while(true) {
 			
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+		
+			
+			status = this.assessHealth();
+			currentTacticIndex = AI.pickTactic(status, currentTacticIndex, tacticLibrary);			
+			healthBeforeTactic = (int) this.getEnergy();
+			
+			while(gameTimer.getSeconds()< 900000)
+			{
+				tacticLibrary[status][currentTacticIndex].run(this);
+			}	
+			
+			AI.gaugeTactic(healthBeforeTactic, (int)this.getEnergy());
+			
 		}
 	}
 
 	/**
 	 * onScannedRobot: What to do when you see another robot
 	 */
-	public void onScannedRobot(ScannedRobotEvent e) {
-		fire(1);
+	public void onScannedRobot(solomon s, ScannedRobotEvent e) {
+		
+		tacticLibrary[status][currentTacticIndex].onScannedRobot_(this, e);
+		
 	}
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		turnLeft(90 - e.getBearing());
+
+		tacticLibrary[status][currentTacticIndex].onHitByBullet_(this, e);
+		
 	}
+	
+	
+	/**
+	 * assess the health of the solomon and returns a status number
+	 * which will be used as an index in acessing the 2D array of tactics
+	 * @return
+	 */
+	private byte assessHealth()
+	{
+		int health = (int) this.getEnergy();
+		byte status = 0;
+		
+		if(health >= E_AGGRESSIVE)
+		{
+			status = 0;
+		}
+		else if((health >= AGGRESSIVE)&&(health < E_AGGRESSIVE))
+		{
+			status = 1;
+		}
+		else if((health >= DEFENSIVE)&&(health < AGGRESSIVE))
+		{
+			status = 2;
+		}
+		else
+		{
+			status = 3;
+		}
+		
+		return status;
+	}
+	
 	
 }
