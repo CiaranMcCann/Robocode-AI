30cd7bd0a7832f1384c550b14d1d34ea7da97adb
CarlQLange <carl@flax.ie>
2010-02-26 12:26:24 +0000

    Added actual tactic, unfinished. Longest-distance calculation. Really needs commenting and preferably a flow chart.

--- Solomon/src/itc/tactic/CTactic_ed0.java
+++ Solomon/src/itc/tactic/CTactic_ed0.java
@@ -1,24 +1,65 @@
 package itc.tactic;
 
-import robocode.HitByBulletEvent;
-import robocode.ScannedRobotEvent;
+import robocode.*;
 import itc.CTactic;
 import itc.solomon;
 
+/**
+ * This needs documentation and better commenting.
+ * TODO: Make this better, damnit.
+ * 
+ * @author carllange
+ *
+ */
 public class CTactic_ed0 extends CTactic {
+	
+	boolean scannedRobotYet = false;
+	double enemyDistance = -1;
+	double furthestPossibleDistance = -1;
+	
 	@Override
 	public void run_(solomon s)
 	{
-		s.ahead(getRandom());
-		s.turnRight(getRandom(360));
+		getFurthestPossibleDistance(s);
+		
+		while (scannedRobotYet == false){
+			s.ahead(getRandom());
+			s.turnRight(getRandom(360));
+		}
+		
+		while (scannedRobotYet == true){
+			
+			// If distance from enemy isn't within an acceptable margin, move to a spot that is.
+			if (acceptableDist() != true)
+			{
+				// TODO: move to a better location.
+			}
+		}
+	}
+
+	private boolean acceptableDist() {
+		// TODO Auto-generated method stub
+		// This could probably be done with bias, and put in CTactic...
+		return false;
 	}
 
 	@Override
-	public void onScannedRobot_(solomon s, ScannedRobotEvent e)
-	{
+	public void onScannedRobot_(solomon s, ScannedRobotEvent e){
+		enemyDistance = e.getDistance();
+		
+		scannedRobotYet = true;
 		fire(s, e.getDistance());
 	}
 	
+	private void getFurthestPossibleDistance(solomon s) {
+		double h = s.getBattleFieldHeight();
+		double w = s.getBattleFieldWidth();
+		
+		double hyp = Math.sqrt((h*h) + (w*w));
+		
+		furthestPossibleDistance = hyp;
+	}
+
 	@Override
 	public void onHitByBullet_(solomon s, HitByBulletEvent e)
 	{
